32e0a2161a51a0a80d0c19828fa9f87fd9b7ac58
Mr-Macko <max.allesch@gmx.de>
2022-03-25 14:29:43 +0100

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,16 +3,20 @@ import { PieChart, Pie, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
-    useEffect(() => { setData(() => getData());}, [events]);
-
-    const getData = () => {
-        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    useEffect(() => {
+      const getData = () => {
+        const genres = ['React', 'JavaScript', 'Node.js', 'jQuery', 'AngularJS'];
+  
         const data = genres.map((genre) => {
-          const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-          return { name: genre, value: value };
+          const value = events.filter(({ summary }) =>
+            summary.split(' ').includes(genre)
+          ).length;
+          return { name: genre, value };
         });
         return data;
       };
+      setData(() => getData());
+    }, [events]);
 
     return (
         <ResponsiveContainer height={400} >
