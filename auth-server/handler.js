f06d2f59d23b3b74937120731940605db46c4fbd
Mr-Macko <max.allesch@gmx.de>
2022-03-25 13:59:24 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,7 +20,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://mr-macko.github.io/meet/"],
-  javascript_origins: ["https://mr-macko.github.io", "http://localhost:3000"],
+  javascript_origins: ["https://mr-macko.github.io/", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
