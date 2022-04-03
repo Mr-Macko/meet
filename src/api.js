88c53e4fb2dd131c67c915693ee2f5bef5d36b53
Mr-Macko <max.allesch@gmx.de>
2022-04-03 16:48:03 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -11,97 +11,98 @@ import NProgress from 'nprogress';
  * The Set will remove all duplicates from the array.
  */
 export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
 };
 
 export const checkToken = async (accessToken) => {
-    const result = await fetch(
-      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    )
-      .then((res) => res.json())
-      .catch((error) => error.json());
-  
-    return result;
-  };
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
 
-  export const getEvents = async () => {
-    NProgress.start();
-  
-    if (window.location.href.startsWith("http://localhost")) {
-      NProgress.done();
-      return mockData;
+  return result;
+};
+
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = 'https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
     }
-  
-    if (!navigator.onLine) {
-      const data = localStorage.getItem("lastEvents");
-      NProgress.done();
-      return data ? JSON.parse(data).events:[];;
-    }
-  
-    const token = await getAccessToken ();
-  
-    if (token) {
-      removeQuery();
-      const url = 'https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
-      const result = await axios.get(url);
-      if (result.data) {
-        var locations = extractLocations(result.data.events);
-        localStorage.setItem("lastEvents", JSON.stringify(result.data));
-        localStorage.setItem("locations", JSON.stringify(locations));
-      }
-      NProgress.done();
-      return result.data.events;
-    }
-  };
+    NProgress.done();
+    return result.data.events;
+  }
+};
 
 export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
+  const accessToken = localStorage.getItem('access_token');
+  console.log({accessToken})
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
-      await localStorage.removeItem("access_token");
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = await searchParams.get("code");
-      if (!code) {
-        const results = await axios.get(
-          "https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-        );
-        const { authUrl } = results.data;
-        return (window.location.href = authUrl);
-      }
-      return code && getToken(code);
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      window.location.href = authUrl;
+      return;
     }
-    return accessToken;
+    return code && getToken(code);
+  }
+  return accessToken;
 }
 
 const removeQuery = () => {
-    if (window.history.pushState && window.location.pathname) {
-      var newurl =
-        window.location.protocol +
-        "//" +
-        window.location.host +
-        window.location.pathname;
-      window.history.pushState("", "", newurl);
-    } else {
-      newurl = window.location.protocol + "//" + window.location.host;
-      window.history.pushState("", "", newurl);
-    }
-  };
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
 
 const getToken = async (code) => {
-const encodeCode = encodeURIComponent(code);
-const { access_token } = await fetch(
-    'https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-)
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://dinpae5atj.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode
+  )
     .then((res) => {
-    return res.json();
+      return res.json();
     })
     .catch((error) => error);
 
-access_token && localStorage.setItem("access_token", access_token);
+  access_token && localStorage.setItem("access_token", access_token);
 
-return access_token;
+  return access_token;
 };
