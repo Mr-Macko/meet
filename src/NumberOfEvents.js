53120a2f2f6837deca5de4f7a9709da2d9bdcd6a
Mr-Macko <max.allesch@gmx.de>
2022-04-03 16:47:32 +0200

    removed duplicate of updateNumberOfEvents function & updated render method

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,42 +2,23 @@ import React, { Component } from 'react';
 import { ErrorAlert } from './Alert';
 
 export class NumberOfEvents extends Component {
-    constructor() {
-        super();
-        // stting the values for the default states
-        this.state = {
-          numberOfEvents: 32,
-          infoText: "",
-        };
-      }
-      
-      updateNumberOfEvents = (event) => {
-        this.setState({
-          numberOfEvents: event.target.value,
-          infoText: "",
-        });
-        if (event.target.value > 0 && event.target.value < 33) {
-          this.props.updateEventNumbers(event.target.value);
-        } else {
-          this.setState({ infoText: "Select a number between 1 and 32." });
-        }
-      };
 
-    render() {
-        return(
-        <div className="NumberOfEvents">
-        <ErrorAlert text={this.state.infoText} />
-            <span>Number of Events </span>
-            <input 
-            type="number" 
-            className="number"
-            value={this.state.numberOfEvents} 
-            onChange={this.updateNumberOfEvents}
-            />
-        </div>
-
-        )
-    }};
-
-
-// export default NumberOfEvents;
+  render() {
+    return (
+      <div className='NumberOfEvents'>
+        {/* <label className='numbersearch-label' for='name'>Number of events:</label> */}
+        <p>Number of Events</p>
+        <br></br>
+        <input
+          name='name'
+          type='number'
+          className='number'
+          min='0'
+          max='51'
+          value={this.props.numberOfEvents}
+          onChange={(e) => this.props.updateNumberOfEvents(e)} />
+        <ErrorAlert text={this.props.errorAlert} />
+      </div>
+    )
+  }
+};
